86820f8d4b940d7b5bf4376d1961769a30830434
Julio Silva <juliocesar.amancio@yahoo.com.br>
2023-07-22 11:04:11 -0300

    chore: change name import newsController in newsRouter

--- src/routes/NewsRouter.ts
+++ src/routes/NewsRouter.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
-import NewsController from "../app/controllers/NewsController";
+import NewController from "../app/controllers/NewsController";
 import AuthMiddleware from "../app/middlewares/AuthMiddleware";
 
 const router = Router();
 
-router.get("/", NewsController.index);
-router.get("/search", NewsController.showParams);
-router.get("/:id", NewsController.show);
-router.patch("/:id", AuthMiddleware.auth, NewsController.update);
-router.delete("/:id", AuthMiddleware.auth, NewsController.delete);
-router.post("/", AuthMiddleware.auth, NewsController.store);
+router.get("/", NewController.index);
+router.get("/search", NewController.showParams);
+router.get("/:id", NewController.show);
+router.patch("/:id", AuthMiddleware.auth, NewController.update);
+router.delete("/:id", AuthMiddleware.auth, NewController.delete);
+router.post("/", AuthMiddleware.auth, NewController.store);
 
 export default router;
